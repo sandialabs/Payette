803a7a2fcf25f4bef26d80a3a5f9689db1e727ed
Tim Fuller <tjfulle@sandia.gov>
2012-09-20 15:22:29 -0600

    bug fix in plastic.f90

--- Source/Materials/Models/Plastic/plastic_eos.f90
+++ Source/Materials/Models/Plastic/plastic_eos.f90
@@ -6,18 +6,18 @@
     dum=ui(1)
     call bombed("eos moduli not enabled")
   end subroutine check_eos
-  subroutine get_eos_moduli(ui, pres, enrg, tmpr, rho, k, g)
+  subroutine eos_moduli(ui, pres, enrg, tmpr, rho, k, g)
     implicit none
     integer, parameter :: dk = selected_real_kind(14)
     real(kind=dk), dimension(*) :: ui
-    real(kind=dk), dimension*(1) :: pres, enrg, tmpr, rho
+    real(kind=dk), dimension(1) :: pres, enrg, tmpr, rho
     real(kind=dk) :: k, g, dum
     dum=ui(1)
-    dum=pres
-    dum=enrg
-    dum=tmpr
-    dum=rho
-    dum=k
-    dum=g
+    dum=pres(1)
+    dum=enrg(1)
+    dum=tmpr(1)
+    dum=rho(1)
+    dum=k(1)
+    dum=g(1)
     call bombed("eos moduli not enabled")
-  end subroutine get_eos_moduli
+  end subroutine eos_moduli
